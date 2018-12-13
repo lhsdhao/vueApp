8b11854db1554e84d67abe9031ea65a63393da99
huasong <1805606711@qq.com>
2018-12-13 14:15:19 +0800

    增加地tabBar

--- src/main.js
+++ src/main.js
@@ -9,10 +9,15 @@ import router from './router.js'
 // 导入mui的样式文件
 import './lib/mui/css/mui.min.css'
 import './lib//mui/fonts/mui.ttf'
+import './lib/mui/css//app.css'
+import './lib/mui/fonts/mui-icons-extra.ttf'
+import './lib/mui/css/icons-extra.css'
+
 
 
 //按需导入mint-Ui组件
 import "mint-ui/lib/style.min.css"
+
 
 import { Header } from 'mint-ui'
 
